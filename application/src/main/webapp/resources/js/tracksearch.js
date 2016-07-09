7755725891c02ab68fc116bfa87df0ed12d5ff58
Dimitri <excaliware@gmail.com>
2016-07-09 18:52:46 +0300

    Add the populateResultsTable method.
    populateResultsTable now create a new row from the template row.
    Refactor the search method.

--- application/src/main/webapp/resources/js/tracksearch.js
+++ application/src/main/webapp/resources/js/tracksearch.js
@@ -1,16 +1,13 @@
 "use strict";
 
-$(document).ready(function () {
+jQuery(document).ready(function () {
 
-    /* Perform search using Ajax. */
+    /* Handle the search form (Ajax). */
     $("#search-form").on("submit", function () {
 
-        if ($("#query").val() == "") {
-            return false;
-        }
         search();
-        return false;
         /* Prevent default */
+        return false;
     });
 
     /* Handle the scroll-to-top link. */
@@ -18,68 +15,85 @@ $(document).ready(function () {
         window.scrollTo(0, 0);
         return false;
     });
-
 });
 /* end $(document).ready() */
 
+/**
+ * Performs an Ajax-based search; populates the results table with found tracks.
+ */
 function search() {
 
+    const MAX_QUERY = 16;
+    const MIN_QUERY = 2;
     var query = $("#query").val();
-    if (query.length > 40) {
-        query = query.substr(0, 40); //Trimming long line
+    /* Do not proceed with queries which are empty or one-character long. */
+    if (query.length < MIN_QUERY) {
+        return;
+    } else if (query.length > MAX_QUERY) {
+        /* Truncate the query string to the maximum allowed size. */
+        query = query.substr(0, MAX_QUERY);
     }
-    var category = $("#search-form select[name=category] option:selected").val();
+
+    var category = $("select[name=category] option:selected").val();
     console.log("Search query: " + query);
     console.log("Category: " + category);
 
-    /* Empty the search results table. */
-    $("#results tr:gt(0)").remove();
+    /* Empty the previous search results, if any. */
+    $("#results tr:gt(1)").remove();
 
     $.ajax({
         type: "GET",
-        url: "/api/search/",
+        url: "/api/search",
         data: {
             query: query,
             category: category
         },
         dataType: "json"
+
     }).fail(function (xhr, status, errorThrown) {
-        $("#status").text("Service is currently unavailable");
-        console.error("There was a problem");
-        console.log("Error: " + errorThrown);
-        console.log("Status: " + status);
-    }).done(function (data) {
-        console.log(data);
-        populateResultsTable(data);
+
+        $("#status-message").text("Service is currently unavailable. Please try again.");
+        console.log(status + ": " + xhr.status + " " + errorThrown);
+
+
+    }).done(function (results) {
+
+        console.log(results);
+        $("#status-message").text("Found: " + results.length);
+        populateResultsTable(results);
     });
 }
 
-function populateResultsTable(data) {
+/**
+ * Inserts the search results into the results table.
+ * @param items the items to be inserted
+ */
+function populateResultsTable(items) {
 
-    $.each(data, function (i, item) {
-        $("#results").append($("<tr>").append(
-            $("<td>").text(item.id),
-            $("<td>").text(item.name),
-            $("<td>").text(item.location)
-        ));
+    $.each(items, function (i, item) {
+        /* Create a new row from the template row, and populate it with the current item. */
+        var $row = $("#row-template").clone().removeAttr("id").show();
+        $row.find("td:eq(0) button:eq(0)").on("click", function() {playPreview(item.id)});
+        $row.find("td:eq(0) button:eq(1)").on("click", function() {addToPlaylist(item.id)});
+        $row.find("td:eq(1)").text(item.name);
+        $row.find("td:eq(2)").text(item.id);
+        $row.find("td:eq(3)").text(item.location);
+        $("#results").append($row);
     });
 }
 
-function ajaxAddSong(AddId) {
-    var header = $("meta[name='_csrf_header']").attr("content");
-    var token = $("meta[name='_csrf']").attr("content");
-    $.ajax({
-        type: "POST",
-        url: "/addSong",
-        data: "songId=" + AddId,
-        beforeSend: function (xhr) {
-            xhr.setRequestHeader(header, token);
-        },
-        success: function () {
-            console.log("Request to add was submitted successfully");
-        },
-        error: function () {
-            alert("Error while request..");
-        }
-    });
+/**
+ * Fetches the track with the given id and plays it.
+ * @param id the id of the track
+ */
+function playPreview(id) {
+    console.log("Play: " + id);
+}
+
+/**
+ * Adds the track with the given id to the current playlist.
+ * @param id the id of the track
+ */
+function addToPlaylist(id) {
+    console.log("Add: " + id);
 }

a0ecc2cdc488cfc096c9f1e125f85b6ce35088ed
Dimitri <excaliware@gmail.com>
2016-07-09 18:58:00 +0300

    Implement the search HTTP endpoint
    Only the search by track 'title' (or currently 'name') is available for now.

--- application/src/main/java/com/musicforall/web/TrackSearchController.java
+++ application/src/main/java/com/musicforall/web/TrackSearchController.java
@@ -1,18 +1,21 @@
 package com.musicforall.web;
 
 import com.musicforall.model.Track;
-import com.musicforall.model.User;
 import com.musicforall.services.track.TrackService;
-import com.musicforall.services.user.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.PostConstruct;
-import javax.inject.Inject;
 import java.util.Collection;
 import java.util.List;
-import java.util.logging.Logger;
 
 /**
  * This RESTful Web service provides the track search facility.
@@ -25,7 +28,7 @@ public class TrackSearchController {
     @Autowired
     private TrackService trackService;
 
-    private static final Logger log = Logger.getLogger("TrackSearchController");
+    private static final Logger log = LoggerFactory.getLogger("TrackSearchController");
 
     @PostConstruct
     private void addSampleRecords() {
@@ -34,10 +37,32 @@ public class TrackSearchController {
         trackService.save(new Track("Track 2", "/track2.mp3"));
     }
 
+    /**
+     * Searches tracks by the specified <code>query</code> in the specified <code>category</code>.
+     */
     @RequestMapping(method = RequestMethod.GET)
-    public Collection<Track> search(@RequestParam String query) {
+    public ResponseEntity search(@RequestParam String query, @RequestParam String category) {
 
-        Collection<Track> tracks = trackService.getAllByName(query);
-        return tracks;
+        /* Limit the query string length. */
+        final int MAX_QUERY = 16;
+        final int MIN_QUERY = 2;
+
+        if (query.length() < MIN_QUERY || query.length() > MAX_QUERY) {
+            return new ResponseEntity(HttpStatus.BAD_REQUEST);
+        }
+
+        List<Track> tracks;
+
+        /* Perform search in the appropriate repository corresponding the category. */
+        switch (category) {
+            case "title":
+                tracks = trackService.getAllByName(query);
+                break;
+            case "artist":
+            case "album":
+            default:
+                return new ResponseEntity(HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<List<Track>>(tracks, HttpStatus.OK);
     }
 }
